17b87c6bed26e88a6d6a98b45956b534b48465c7
unknown <salimoyinlola@gmail.com>
2023-05-12 04:25:23 +0100

    Initial Commit

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 from dotenv import load_dotenv
 load_dotenv()
 import requests, os, uuid, json
 from flask import Flask, redirect, url_for, request, render_template, session
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+
 
 app = Flask(__name__)
 
 language_key = os.getenv("LANGUAGE_KEY")
 language_endpoint = os.getenv("LANGUAGE_ENDPOINT")
+
+subscription_key = os.getenv("SUBSCRIPTION_KEY")
+endpoint = os.getenv("ENDPOINT")
+credentials1 = CognitiveServicesCredentials(subscription_key)
+client1 = ComputerVisionClient(endpoint, credentials1)
+
+
 def authenticate_client():
     ta_credential = AzureKeyCredential(language_key)
     text_analytics_client = TextAnalyticsClient(
@@ -24,84 +35,83 @@ def index():
     return render_template('index.html')
 
 # Function 1
-@app.route('/function1', methods=['GET', 'POST'])
+@app.route('/sentimentanalysis', methods=['GET', 'POST'])
 def function1():
     if request.method == 'POST':
-        # Get user input
-        text = request.form['text']
 
-        # Perform function 1 on the input
-        result = function1_logic(text)
+        original_text = request.form['text']
+        documents = [original_text]
 
-        return render_template('function1.html', result=result)
+        result = client.analyze_sentiment(documents, show_opinion_mining=True)
+        doc_result = [doc for doc in result if not doc.is_error]
+
+        positive_reviews = [doc for doc in doc_result if doc.sentiment == "positive"]
+        negative_reviews = [doc for doc in doc_result if doc.sentiment == "negative"]
+
+        positive_mined_opinions = []
+        mixed_mined_opinions = []
+        negative_mined_opinions = []
+
+        for document in doc_result:
+            sentiment = document.sentiment
+            positive_confidence_scores = document.confidence_scores.positive
+            negative_confidence_scores = document.confidence_scores.negative
+            neutral_confidence_scores = document.confidence_scores.neutral
+        return render_template(
+            'function1.html', 
+            sentiment = sentiment, 
+            positive_confidence_scores = positive_confidence_scores,
+            negative_confidence_scores = negative_confidence_scores,
+            neutral_confidence_scores = neutral_confidence_scores
+            )
     return render_template('function1.html')
 
 # Function 2
-@app.route('/function2', methods=['GET', 'POST'])
+@app.route('/keyphraseextration', methods=['GET', 'POST'])
 def function2():
     if request.method == 'POST':
         # Get user input
-        text = request.form['text']
+        #text = request.form['text']
 
         # Perform function 2 on the input
-        result = function2_logic(text)
+        #result = function2_logic(text)
 
-        return render_template('function2.html', result=result)
+        original_text = request.form['text']
+        documents = [original_text]
+        for n in range(0, len(documents)):
+            response = client.extract_key_phrases(documents)[n]
+        for phrase in response.key_phrases:
+            phrase = phrase
+        return render_template(
+            'function2.html', 
+            documents = documents,
+            phrase = phrase
+            )
+
+        #return render_template('function2.html', result=result)
     return render_template('function2.html')
 
 # Function 3
-@app.route('/function3', methods=['GET', 'POST'])
+@app.route('/imageanalysis', methods=['GET', 'POST'])
 def function3():
     if request.method == 'POST':
         # Get user input
-        text = request.form['text']
+        image_url = request.form.get('image_url')
 
-        # Perform function 3 on the input
-        result = function3_logic(text)
 
-        return render_template('function3.html', result=result)
+        # Call the Azure Cognitive Vision API to analyze the image
+        result = client1.analyze_image(image_url, visual_features=['Categories', 'Description'])
+
+        # Process the result as per your application's needs
+        categories = result.categories
+        description = result.description
+
+        return render_template('function3.html', 
+                               categories=categories, 
+                               description=description
+                               )
+
     return render_template('function3.html')
-
-# Function 4
-@app.route('/function4', methods=['GET', 'POST'])
-def function4():
-    if request.method == 'POST':
-        # Get user input
-        text = request.form['text']
-
-        # Perform function 4 on the input
-        result = function4_logic(text)
-
-        return render_template('function4.html', result=result)
-    return render_template('function4.html')
-
-# Function 1 logic
-def function1_logic(text):
-    # Perform function 1 operations on the text
-    # Replace this with your actual function logic
-    result = text.upper()
-    return result
-
-# Function 2 logic
-def function2_logic(text):
-    # Perform function 2 operations on the text
-    # Replace this with your actual function logic
-    result = text.lower()
-    return result
-
-# Function 3 logic
-def function3_logic(text):
-    # Perform function 3 operations on the text
-    # Replace this with your actual function logic
-    result = text[::-1]  # Reverse the text
-    return result
-
-# Function 4 logic
-def function4_logic(text):
-    # Perform function 4 operations on the text
-    # Replace this with your actual function logic
-    result = text + '!'
-    return result
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
